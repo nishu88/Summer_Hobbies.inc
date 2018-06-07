3655c23360397db3e7ed96611f4e266d2f721734
nishu88 <nishanthdilip@yahoo.co.in>
2018-06-07 18:58:43 +0530

    changes to java file

--- Android App Dev/JustJava_CoffeeOrderingApp/app/src/main/java/com/nishu/interrupt/zero/fr3efall/justjava_coffeeorderingapp/MainActivity.java
+++ Android App Dev/JustJava_CoffeeOrderingApp/app/src/main/java/com/nishu/interrupt/zero/fr3efall/justjava_coffeeorderingapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.nishu.interrupt.zero.fr3efall.justjava_coffeeorderingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +42,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void orderClicked(View view) {
+        TextView amt_text_view=(TextView) findViewById(
+                R.id.bill);
+        String order="Name: Fr3EfaLL \n";
+        order=order+"Quantity: "+quantity;
+        amt_text_view.setText(order);
+
+        TextView thankYou_text_view=(TextView) findViewById(
+                R.id.thankYou);
+        thankYou_text_view.setText("Thank you, Visit Again!!!");
+
+        Button but=(Button) findViewById(
+                R.id.order_button);
+        but.setText("Order Complete");
+
 
     }
 
